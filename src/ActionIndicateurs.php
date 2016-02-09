5199b148b2ac979328c6e025ad0cba920910a769
fabien <fabien.pelisson@innovisite.com>
2016-02-09 17:31:37 +0100

    Delete operation on counting period

--- src/ActionIndicateurs.php
+++ src/ActionIndicateurs.php
@@ -2,6 +2,67 @@
 
 include_once("Config.inc");
 
+function showCountingData($dates, $period, $cpList, $fcp, $excluded = null)
+{
+  $str = sprintf("%2s%20s%5s%5s%5s%5s%5s%5s\n%'-52s\n", 
+		 "id", "start", "v0", "c0", "e0",
+		 "v1", "c1", "e1", "-");
+  $total = array( "value0" => 0, "nbcumul0" => 0, "nbexpected0" => 0,
+		  "value1" => 0, "nbcumul1" => 0, "nbexpected1" => 0 );
+  
+  for($i = 0; $i < count($dates); $i++)
+    {
+      for($j = 0; $j < count($cpList); $j++)
+	{
+	  if(($excluded === null) or !in_array($cpList[$j]["cpId"], $excluded))
+	    {
+	      $indicator = $fcp->getIndicator($cpList[$j]["cpId"]);
+	      $data = $indicator->getValues($cpList[$j]["cpId"],
+					    mkTimeFromString($dates[$i]),
+					    $period["hstart"],
+					    $period["hend"]);
+	      if(is_array($data)) 
+		{
+		  foreach($data as $v)
+		    {
+		      $str .= sprintf("%2s%20s%5d%5d%5d%5d%5d%5d\n",
+				      $v["id"], $v["start"], $v["value0"],
+				      $v["nbcumul0"], $v["nbexpected0"],
+				      $v["value1"], $v["nbcumul1"], $v["nbexpected1"]);
+		      foreach(array_keys($total) as $key)
+			{
+			  $total[$key] += $v[$key];
+			}
+		    }
+		}
+	    }
+	}
+    }
+  $str .= sprintf("%'-52s\n%22s%5d%5d%5d%5d%5d%5d\n",
+		  "-", "Total = ", 
+		  $total["value0"], $total["nbcumul0"], $total["nbexpected0"],
+		  $total["value1"], $total["nbcumul1"], $total["nbexpected1"]);
+  return $str;
+}
+
+function deleteCountingData($dates, $period, $cpList, $fcp, $excluded = null)
+{
+  for($i = 0; $i < count($dates); $i++)
+    {
+      for($j = 0; $j < count($cpList); $j++)
+	{
+	  if(($excluded === null) or !in_array($cpList[$j]["cpId"], $excluded))
+	    {
+	      $indicator = $fcp->getIndicator($cpList[$j]["cpId"]);
+	      $indicator->deleteValues($cpList[$j]["cpId"],
+				       mkTimeFromString($dates[$i]),
+				       $period["hstart"],
+				       $period["hend"]);
+	    }
+	}
+    }
+}
+
 try {
 
   if(count($argv) < 7)
@@ -56,13 +117,6 @@ try {
 				       'year' => 0 )
 				);
 
-
-  include_once("BluePHP/BTopLocalServer/FlowCountingProcessing.inc");
-  $fcp = new FlowCountingProcessing();
-  $fcpList = $fcp->getFlowsFromCounter($ref);
-
-  $flowData = $fcp->getIndicator();
-
   if($action == "date")
     {
       echo sprintf("\n%10s%9s%9s\n%'-28s\n",
@@ -77,42 +131,33 @@ try {
     }
   else if($action == "show")
     {      
-      echo sprintf("%2s%20s%5s%5s%5s%5s%5s%5s\n%'-52s\n", 
-		   "id", "start", "v0", "c0", "e0",
-		   "v1", "c1", "e1", "-");
-      $total = array( "value0" => 0, "nbcumul0" => 0, "nbexpected0" => 0,
-		      "value1" => 0, "nbcumul1" => 0, "nbexpected1" => 0 );
+      include_once("BluePHP/BTopLocalServer/FlowCountingProcessing.inc");
+      $fcp = new FlowCountingProcessing();
+      $fcpList = $fcp->getFlowsFromCounter($ref);
+      
+      echo showCountingData($res, $period, $fcpList, $fcp);
 
-      for($i = 0; $i < count($res); $i++)
-	{
-	  $data = $flowData->getValues($fcpList[0]["fcpId"],
-				       mkTimeFromString($res[$i]),
-				       $period["hstart"],
-				       $period["hend"]);
-	  if(is_array($data)) 
-	    {
-	      foreach($data as $v)
-		{
-		  echo sprintf("%2s%20s%5d%5d%5d%5d%5d%5d\n",
-			       $v["id"], $v["start"], $v["value0"],
-			       $v["nbcumul0"], $v["nbexpected0"],
-			       $v["value1"], $v["nbcumul1"], $v["nbexpected1"]);
-		  foreach(array_keys($total) as $key)
-		    {
-		      $total[$key] += $v[$key];
-		    }
-		}
-	    }
-	}
-      echo sprintf("%'-52s\n%22s%5d%5d%5d%5d%5d%5d\n",
-		   "-", "Total = ", 
-		   $total["value0"], $total["nbcumul0"], $total["nbexpected0"],
-		   $total["value1"], $total["nbcumul1"], $total["nbexpected1"]);
+      include_once("BluePHP/BTopLocalServer/ZoneCounting.inc");
+      $zcp = new ZoneCounting();
+      $zcpList = $zcp->getZonesFromCounter($ref);
+
+      echo showCountingData($res, $period, $zcpList, $zcp, $zcpExcluded[$clientId]);
+    }
+  else if($action == "delete")
+    {
+      include_once("BluePHP/BTopLocalServer/FlowCountingProcessing.inc");
+      $fcp = new FlowCountingProcessing();
+      $fcpList = $fcp->getFlowsFromCounter($ref);
+      
+      deleteCountingData($res, $period, $fcpList, $fcp);
+
+      include_once("BluePHP/BTopLocalServer/ZoneCounting.inc");
+      $zcp = new ZoneCounting();
+      $zcpList = $zcp->getZonesFromCounter($ref);
+
+      deleteCountingData($res, $period, $zcpList, $zcp, $zcpExcluded[$clientId]);      
     }
 
-  include_once("BluePHP/BTopLocalServer/ZoneCounting.inc");
-  $zcp = new ZoneCounting();
-  $zcpList = $zcp->getZonesFromCounter($ref);
 }
 catch(Exception $e)
 {
